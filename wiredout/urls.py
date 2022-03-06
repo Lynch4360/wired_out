51492941da33e9172be4d4495913c1f69941e52e
Lynch4360 <lynch4360@gmail.com>
2022-03-06 04:33:16 +0000

    Add registration form template and url

--- wiredout/urls.py
+++ wiredout/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     path('summernote/', include('django_summernote.urls')),
     path('', include('newsite.urls'), name='newsite_urls'),
 ]
