7d58efa2ada17207fa6e2c45fea79902718cc841
Lynch4360 <lynch4360@gmail.com>
2022-03-06 06:15:19 +0000

    Create view for register, display flash message on success

--- users/views.py
+++ users/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
+# flash messages display alert to template that disapppear on the next request
 def register(request):
-    form = UserCreationForm()
+    # if we get a post request it creates a form with that post data
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+    # otherwise it will create a blank form
+    else:
+        form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
