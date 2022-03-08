b3dd9ce2e29e3ffa965c0449c0db6875e30abd72
Lynch4360 <lynch4360@gmail.com>
2022-03-08 08:37:10 +0000

    refactor home view, add post details view, url and template

--- newsite/urls.py
+++ newsite/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from newsite import views as newsite_views
 
 urlpatterns = [
-    path('', views.home, name="site-home"),
-    path('about/', views.about, name="site-about"),
+    path('', newsite_views.home.as_view(), name="site-home"),
+    path('detail/<int:pk>', newsite_views.postDetail.as_view(),
+         name="post-detail"),
+    path('about/', newsite_views.about, name="site-about"),
 ]
--- newsite/views.py
+++ newsite/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
+class home(ListView):
+    model = Post
+    template_name = 'home.html'
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'home.html', context)
+
+class postDetail(DetailView):
+    model = Post
+    template_name = 'postDetails.html'
+
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'home.html', context)
 
 
 def about(request):
