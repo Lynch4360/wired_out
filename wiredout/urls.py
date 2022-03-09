f346afa51d8d0306e7bf231b764ffcfe2d2a1f63
Lynch4360 <lynch4360@gmail.com>
2022-03-09 15:28:19 +0000

    Import auth views and make url for login and out

--- wiredout/urls.py
+++ wiredout/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 
@@ -8,4 +9,7 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('summernote/', include('django_summernote.urls')),
     path('', include('newsite.urls'), name='newsite_urls'),
+    path('login/', auth_views.Login.as_view(), name='login'),
+    path('logout/', auth_views.Logout.as_view(), name='logout'),
+
 ]
