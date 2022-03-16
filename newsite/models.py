f4223bd0bec6fbf427d591be18b48d691e0e02a8
Lynch4360 <lynch4360@gmail.com>
2022-03-16 01:43:13 +0000

    Add comment model to models.py fil

--- newsite/models.py
+++ newsite/models.py
@@ -46,5 +46,11 @@ class Post(models.Model):
         return reverse('post-detail', args=[self.id])
 
 
-# class Comment(models.Model):
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True, blank=True)
 
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
